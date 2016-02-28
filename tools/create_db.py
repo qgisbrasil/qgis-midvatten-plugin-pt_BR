c35e5067e1778f2b1d1c5795aacabb4fb8e14318
QGISBrasil <comunidade@qgisbrasil.org>
2016-02-27 23:05:43 -0300

    Update create_db.py

--- tools/create_db.py
+++ tools/create_db.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: iso-8859-1 -*-
 """
 /***************************************************************************
  This part of the Midvatten plugin creates a new "midvatten project plugin". 
